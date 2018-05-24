d763ef4c59e378e77ddd262cb1f0977d62dac761
Mark Raasveldt <mark.raasveldt@gmail.com>
2018-05-24 13:48:02 +0200

    Clone repositories if they do not exist.

--- compile.sh
+++ compile.sh
@@ -9,6 +9,19 @@ else
 	cat configuration.sh >> ~/.bashrc;
 fi
 
+if [ ! -d cub ]
+then
+	git clone https://github.com/NVlabs/cub
+	cd cub
+	git reset --hard c3cceac115c072fb63df1836ff46d8c60d9eb304
+	cd ..
+fi
+
+if [ ! -d cuda-api-wrappers ]
+then
+	git clone https://github.com/eyalroz/cuda-api-wrappers
+fi
+
 # build cuda-api-wrappers
 cd cuda-api-wrappers
 if [ ! -d "$lib" ] 

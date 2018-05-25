b2e9055b5798b951f5b7b9659a8efe8b0e12435b
diegomestre2 <diegogomest@gmail.com>
2018-05-25 14:36:39 +0200

    Non-working baseline

--- include/kernel.hpp
+++ include/kernel.hpp
@@ -2,7 +2,9 @@
 #include "helper.hpp"
 #include "dates.hpp"
 #include "../expl_comp_strat/common.hpp"
+#include <cooperative_groups.h>
 
+using namespace cooperative_groups;
 using u64_t = unsigned long long int;
 namespace cuda{
   
@@ -12,6 +14,36 @@ namespace cuda{
 	    int wid = global_warp_id();
 	    int lid = warp_local_thread_id();
 	    printf(" Global Warp: %d Local Warp: %d \n", wid, lid);
+	}
+	__device__ 
+	/*int atomicAggInc_primitives(int *ctr) {
+	  	unsigned int active = __activemask();
+	  	int leader = __ffs(active) - 1;
+	  	int change = __popc(active);
+	  	unsigned int rank = __popc(active & __lanemask_lt());
+	  	int warp_res;
+	  	if(rank == 0)
+	    	warp_res = atomicAdd(ctr, change);
+	  	warp_res = __shfl_sync(active, warp_res, leader);
+	  	return warp_res + rank;
+	}*/
+
+	__device__ 
+	int atomicAggInc(int *ctr) {
+		auto g = coalesced_threads();
+		int warp_res;
+		if(g.thread_rank() == 0)
+			warp_res = atomicAdd(ctr, g.size());
+		return g.shfl(warp_res, 0) + g.thread_rank();
+	}
+
+	__global__ 
+	void filter_k(int *dst, int *nres, const int *src, int n) {
+  		int i = threadIdx.x + blockIdx.x * blockDim.x;
+  		if(i >= n)
+    		return;
+  		if(src[i] > 0)
+    		dst[atomicAggInc(nres)] = src[i];
 	}
 
 	__inline__ __device__
@@ -83,10 +115,56 @@ namespace cuda{
 	            atomicAdd((u64_t*)&(aggregations[idx].count), (u64_t)1);
 	            
 	        }
-	        __syncthreads();
+	        //__syncthreads();
 	    //}
 
 	}
 
+		__global__
+	void blockwise01_tpchQ01(int *shipdate, int *discount, int *extendedprice, int *tax, 
+		char *returnflag, char *linestatus, int *quantity, AggrHashTable *aggregations, size_t cardinality){
 
+		u64_t i = blockIdx.x * blockDim.x + threadIdx.x * 32;
+		u64_t next = min( (u64_t) cardinality, i + 32);
+		if(i >= cardinality)
+    		return;
+
+    	// u64_t local_quantity = 0;
+    	// u64_t local_base_price = 0;
+    	// u64_t local_disc_price = 0;
+    	// u64_t local_charge = 0;
+    	// u64_t local_disc = 0;
+    	// u64_t local_count = 0;
+		for(; i < next; i++) {
+	  		if(shipdate[i] <= todate_(2, 9, 1998)){
+	    		//dst[atomicAggInc(nres)] = src[i];
+	            const auto disc = discount[i];
+	            const auto price = extendedprice[i];
+	            const auto disc_1 = Decimal64::ToValue(1, 0) - disc;
+	            const auto tax_1 = tax[i] + Decimal64::ToValue(1, 0);
+	            const auto disc_price = Decimal64::Mul(disc_1, price);
+	            const auto charge = Decimal64::Mul(disc_price, tax_1);
+	            const idx_t idx = returnflag[i] << 8 | linestatus[i];
+		     
+	            atomicAdd((u64_t*)&(aggregations[idx].sum_quantity), (u64_t) quantity[i]);
+	            atomicAdd((u64_t*)&(aggregations[idx].sum_base_price), (u64_t)price);
+	            atomicAdd((u64_t*)&(aggregations[idx].sum_disc_price), (u64_t)int128_add64(aggregations[idx].sum_disc_price, disc_price));
+	            atomicAdd((u64_t*)&(aggregations[idx].sum_charge), (u64_t)int128_add64(aggregations[idx].sum_charge, charge));
+	            atomicAdd((u64_t*)&(aggregations[idx].sum_disc), (u64_t)disc);
+	            atomicAdd((u64_t*)&(aggregations[idx].count), (u64_t)1);
+				// local_quantity += (u64_t) quantity[i];
+				// local_base_price += (u64_t) price;
+				// local_disc_price += (u64_t) disc_price;
+				// local_charge += (u64_t) charge;
+				// local_disc += (u64_t) disc;
+				// local_count += (u64_t) 1;
+			}
+		}
+        // atomicAdd((u64_t*)&(aggregations[idx].sum_quantity), local_quantity);
+        // atomicAdd((u64_t*)&(aggregations[idx].sum_base_price), local_base_price);
+        // atomicAdd((u64_t*)&(aggregations[idx].sum_disc_price), local_disc_price);
+        // atomicAdd((u64_t*)&(aggregations[idx].sum_charge), local_charge);
+        // atomicAdd((u64_t*)&(aggregations[idx].sum_disc), local_disc);
+        // atomicAdd((u64_t*)&(aggregations[idx].count), local_count);
+	}
 }
--- src/main.cu
+++ src/main.cu
@@ -39,6 +39,7 @@ int main(){
     auto size_char         = data_length * sizeof(char);
     auto size_aggregations = MAX_GROUPS  * sizeof(AggrHashTable);
 
+    auto start = timer::now();
     /* Transfer data to device */
     cuda::memory::copy(d_shipdate.get(),      shipdate,      size_int);
     cuda::memory::copy(d_discount.get(),      discount,      size_int);
@@ -51,14 +52,13 @@ int main(){
 
     /* Setup to launch kernel */
     uint32_t warp_size = 32;
-    uint32_t block_size = 1024;
     uint32_t elements_per_thread = warp_size;
+    uint32_t block_size = 1024 / elements_per_thread;
     uint32_t elements_per_block = block_size * elements_per_thread;
     uint32_t block_cnt = (data_length + block_size - 1) / block_size;
 
     /* Launching Kernel */
-    std::cout << "launch params: <<<" << block_cnt << "," << block_size << ">>>" << std::endl;
-    auto start = timer::now();
+    //std::cout << "launch params: <<<" << block_cnt << "," << block_size << ">>>" << std::endl;
     cuda::naive_tpchQ01<<<block_cnt, block_size>>>(d_shipdate.get(), 
         d_discount.get(), d_extendedprice.get(), d_tax.get(), d_returnflag.get(), 
         d_linestatus.get(), d_quantity.get(), d_aggregations.get(), data_length);
@@ -76,10 +76,10 @@ int main(){
             size_t i = group;
 
             printf("# %c|%c", rf, ls);
-            print_dec("|", aggrs0[i].sum_quantity);
-            print_dec("|", aggrs0[i].sum_base_price);
-            print_dec("|", aggrs0[i].sum_disc_price);
-            print_dec("|", aggrs0[i].sum_charge);
+            print_dec(" | ", aggrs0[i].sum_quantity);
+            print_dec(" | ", aggrs0[i].sum_base_price);
+            print_dec(" | ", aggrs0[i].sum_disc_price);
+            print_dec(" | ", aggrs0[i].sum_charge);
             printf("|%ld\n", aggrs0[i].count);
         }
     }
@@ -93,9 +93,9 @@ int main(){
     std::cout << "\n+-------------------------------------------------------------------------------+\n";
     std::cout << "| TPC-H Q01 performance               : " << std::fixed << tuples_per_second << " [tuples/sec]" << std::endl;
     uint64_t cache_line_size = 128; // bytes
-    std::cout << "| Effective memory throughput (probe) : ~" << std::setprecision(2)
+    std::cout << "| Effective memory throughput (query) : ~" << std::setprecision(2)
               << effective_memory_throughput << " [GB/s]" << std::endl;
-    std::cout << "| Estimated memory throughput (probe) : ~" << std::setprecision(2)
+    std::cout << "| Estimated memory throughput (query) : ~" << std::setprecision(2)
               << (tuples_per_second * cache_line_size / 1024.0 / 1024.0 / 1024.0) << " [GB/s]" << std::endl;
     std::cout << "| Effective memory throughput (read)  : ~" << std::setprecision(2)
               << effective_memory_throughput_read << " [GB/s]" << std::endl;
@@ -105,14 +105,21 @@ int main(){
     std::cout << "| Effective Bandwidth [GB/s]          : " << data_length * 25 * 13 / duration.count() << std::endl;
     std::cout << "+-------------------------------------------------------------------------------+\n";
     /* Test */
-    /*std::vector<int> in(64, 1); 
-    int  *d_in, *d_out;
+    std::cout << std::endl;
+    std::vector<int> in(64, 1);
+    std::vector<int> res(64, 0);  
+    int  *d_in, *d_out, *d_res;
     cudaMalloc(&d_in,       64 * sizeof(int));
     cudaMalloc(&d_out,      64 * sizeof(int));
+    cudaMalloc(&d_res,      64 * sizeof(int));
     cudaMemcpy(d_in,      &in[0],      64 * sizeof(int), cudaMemcpyHostToDevice);
-    cuda::deviceReduceKernel<<<2,32>>>(d_in, d_out, 64);
-    cuda::deviceReduceKernel<<<1,32>>>(d_out, d_out, 2);
+    cudaMemcpy(d_res,      &res[0],      64 * sizeof(int), cudaMemcpyHostToDevice);
+    cuda::filter_k<<<2,32>>>(d_out,d_res, d_in, 64);
+    cudaMemcpy(     &res[0], d_res,      64 * sizeof(int), cudaMemcpyDeviceToHost);
+    for(auto &i : res)
+            printf(" %d ", i);
+    //cuda::deviceReduceKernel<<<1,32>>>(d_out, d_out, 2);
     cudaFree(d_in);
-    cudaFree(d_out);*/
+    cudaFree(d_out);
 
 }

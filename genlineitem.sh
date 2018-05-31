9c4b7eab4346c54ce4c6eac2ffd48ffab6665f9d
Mark Raasveldt <mark.raasveldt@gmail.com>
2018-05-31 11:38:56 +0200

    Script to generate lineitem table.

---
+++ genlineitem.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+if [ ! -d tpch-dbgen ]
+then
+	git clone https://github.com/eyalroz/tpch-dbgen
+fi
+cd tpch-dbgen
+cmake -G "Unix Makefiles" .
+make
+./dbgen -f -T L -s 1
+mv lineitem.tbl ..
